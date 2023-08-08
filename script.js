68afd950b3f84afdec5c925413a0af99920d1843
IIIgorMoura <igor2007moura@gmail.com>
2023-08-08 15:15:56 -0300

    semifinais

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function mostrarMenu() {
     var menu = document.getElementById("opc");
     var icone = document.getElementById("icone");
-    var header = document.getElementById("header");
 
     if (getComputedStyle(menu).display == 'none') {
-        header.classList.remove("header")
-        header.classList.add("headerCell")
+    
         menu.style.display = "flex";
+
+        opc.style.top = "230px"
+
         icone.classList.remove("fa-bars");
         icone.classList.add("fa-times");
     }
 
     else {
         menu.style.display = "none";
+
         icone.classList.remove("fa-times");
         icone.classList.add("fa-bars")
     }
